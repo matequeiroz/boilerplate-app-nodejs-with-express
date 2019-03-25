1083236acf4beae05d6624859af9fe32b475cee0
Mateus Roberto Queiroz Da Silva <mateus.queiroz@qualinfo.com.br>
2019-03-25 15:52:43 -0300

    add dotenv

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
 
 app.use(routes);
 
-app.listen(3000);
+app.listen(process.env.PORT);
